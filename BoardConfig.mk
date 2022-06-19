46fcd93d42bc15a625de1b46ec0160c17bf7a43b
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-20 03:20:16 +0800

    uter: Commonize kernel cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,6 @@ TARGET_SCREEN_DENSITY := 400
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
-BOARD_KERNEL_CMDLINE += earlycon=msm_hsl_uart,0x78af000
 TARGET_KERNEL_CONFIG := uter_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/uter
 
